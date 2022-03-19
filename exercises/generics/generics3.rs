769c163dffa45b8773f113db9d93c0d962d4ed79
Vishal Talwar <rawlatv@gmail.com>
2022-03-19 19:21:51 +0000

    Finished generics

--- exercises/generics/generics3.rs
+++ exercises/generics/generics3.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 // An imaginary magical school has a new report card generation system written in Rust!
 // Currently the system only supports creating report cards where the student's grade
 // is represented numerically (e.g. 1.0 -> 5.5).
@@ -12,13 +14,13 @@
 
 // I AM NOT DONE
 
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -46,7 +48,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
